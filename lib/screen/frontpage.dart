18977e7fee60f439edb2b666037d9785ab24c3e0
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2022-03-08 16:29:10 +0900

    FrontPage作成

---
+++ lib/screen/frontpage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+
+
+class FrontPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Make_Course"),
+        backgroundColor: Colors.blue,
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          child: SizedBox(
+            width: 300,
+            height: 100,
+            child: ElevatedButton(
+              child:const Text(
+                  "コースを作る",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              onPressed: (){
+
+              },
+            ),
+          ),
+
+        ),
+      ),
+    );
+
+  }
+}
