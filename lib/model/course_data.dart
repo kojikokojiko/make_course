f08a06bed2e153797ec87540954e706dd0e00c07
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2022-03-16 18:15:51 +0900

    providerで緯度経度取得、

--- lib/model/course_data.dart
+++ lib/model/course_data.dart
@@ -4,6 +4,9 @@ import 'package:flutter/foundation.dart';
 class CourseData extends ChangeNotifier {
    double? _wind_direct=0;
    double? _course_length=0;
+   double? _latitude=0;
+   double? _longtitude=0;
+
 
 
   void set_wind_direct(double? Windspeed){
@@ -16,8 +19,29 @@ class CourseData extends ChangeNotifier {
       notifyListeners();
    }
 
+
+   void set_latitude(double? Latitude){
+     _latitude=Latitude;
+     notifyListeners();
+   }
+
+   void set_longtitude(double? Longtitude){
+     _longtitude=Longtitude;
+     notifyListeners();
+   }
+
    double? get wd{
     double? aa=_wind_direct;
     return aa;
+   }
+
+   double? get long{
+     double? aa=_longtitude;
+     return aa;
+   }
+
+   double? get lat{
+     double? aa=_latitude;
+     return aa;
    }
 }
--- lib/screen/firstpointpage.dart
+++ lib/screen/firstpointpage.dart
@@ -1,18 +1,95 @@
 import 'package:flutter/material.dart';
 import "package:provider/provider.dart";
 import 'package:make_course/model/course_data.dart';
+import 'package:geolocator/geolocator.dart';
 
-class FirstPointPage extends StatelessWidget {
+class FirstPointPage extends StatefulWidget {
+  @override
+  State<FirstPointPage> createState() => _FirstPointPageState();
+}
+
+class _FirstPointPageState extends State<FirstPointPage> {
+  String _location = "no data";
+  String _lat = "no data";
+  String _long = "no data";
+  double _dlat=0;
+  double _dlong=0;
+
+  Future<void> getLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // 北緯がプラス。南緯がマイナス
+    print("緯度: " + position.latitude.toString());
+    // 東経がプラス、西経がマイナス
+    print("経度: " + position.longitude.toString());
+
+    setState(() {
+      _lat = position.latitude.toString();
+      _long = position.longitude.toString();
+      _dlat = position.latitude;
+      _dlong = position.longitude;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final CourseData data=Provider.of<CourseData>(context,listen: false);
-    print("dfd");
+    final CourseData data = Provider.of<CourseData>(context);
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Container(
-
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text("上マークを設置"),
+        backgroundColor: Colors.blue,
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "ボタンを押して現在地を取得",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              "緯度 $_lat",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              "経度 $_long",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text(
+                "下マークを設置",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(15),
+                primary: Colors.blue,
+                onPrimary: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)
+                )
+                
+              ),
+              onPressed: () {
+                data.set_latitude(_dlat);
+                data.set_longtitude(_dlong);
+                print("pp");
+                print(data.long);
+                print(data.lat);
+              },
+            )
+          ],
+        )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getLocation,
+        child: Icon(Icons.location_on),
+      ),
+    );
   }
 }
