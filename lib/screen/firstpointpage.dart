4425e48e213ec72ce956614c66f6269f839cf796
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2022-03-16 16:50:59 +0900

    flutter androidsdk kotlinのバージョンを変更。一応動く状態にはなった

--- lib/screen/firstpointpage.dart
+++ lib/screen/firstpointpage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import "package:provider/provider.dart";
 import 'package:make_course/model/course_data.dart';
-import ""
+
 class FirstPointPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
